8088bb79defc9242e297db9d6721d22976dcac5f
Vinay K Rajith <vinaykrajith@gmail.com>
2026-03-05 14:26:37 +0530

    fix: hardcode API_BASE URL, remove env variable

--- frontend/src/api/search.ts
+++ frontend/src/api/search.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = (import.meta.env.VITE_API_BASE || "https://lmsai.entab.info").replace(/\/+$/, "");
+const API_BASE = "https://lmsai.entab.info";
 
 export interface SearchResult {
     vc_id: number;       // LMSVideoContents.VCId (PK)
